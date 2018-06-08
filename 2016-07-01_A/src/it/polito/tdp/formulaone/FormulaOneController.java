d1028c7d11a60e1e8eea8749b8c2b110bbb38b99
Daniele Bianco <Daniele Bianco@F550L.station>
2018-06-09 00:23:08 +0200

    Primo e secondo punto

--- 2016-07-01_A/src/it/polito/tdp/formulaone/FormulaOneController.java
+++ 2016-07-01_A/src/it/polito/tdp/formulaone/FormulaOneController.java
@@ -1,9 +1,12 @@
 package it.polito.tdp.formulaone;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.formulaone.model.Driver;
 import it.polito.tdp.formulaone.model.Model;
+import it.polito.tdp.formulaone.model.Season;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -21,7 +24,7 @@ public class FormulaOneController {
     private URL location;
 
     @FXML
-    private ComboBox<?> boxAnno;
+    private ComboBox<Season> boxAnno;
 
     @FXML
     private TextField textInputK;
@@ -31,12 +34,51 @@ public class FormulaOneController {
 
     @FXML
     void doCreaGrafo(ActionEvent event) {
-
+    		
+    	try {
+    		
+    		Season s = (Season) boxAnno.getValue();
+    		if(s == null) {
+    			txtResult.setText("Selezionare una stagione!");
+    			return;
+    		}
+    		model.creaGrafo(s);
+    		Driver d = model.getBestDriver();
+    		txtResult.setText(d.toString());
+    		
+    	} catch(RuntimeException e) {
+    		e.printStackTrace(); // poi commenta
+    		System.out.println("Errore connessione al DB");
+    		txtResult.setText("Errore connessione al DB");
+    	}
+    	
     }
 
     @FXML
     void doTrovaDreamTeam(ActionEvent event) {
+    	try {
+			try {
+				
+				int k = Integer.parseInt(textInputK.getText());
+				if (k <= 0) {
+					txtResult.setText("inserire K > 0");
+					return;
+				}
+				
+				List<Driver> drivers = model.getDreamTeam(k);
+				txtResult.setText(drivers.toString());
+			
+			} catch (NumberFormatException e) {
+				txtResult.setText("inserire K > 0");
+				return;
+			}
+			
 
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			System.out.println("Errorre di connessione al DB");
+			txtResult.setText("Errore di connessine al DB!");
+}
     }
 
     @FXML
@@ -49,5 +91,7 @@ public class FormulaOneController {
     
     public void setModel(Model model){
     	this.model = model;
+    	this.boxAnno.getItems().addAll(model.getAllSeason());
+    	
     }
 }
